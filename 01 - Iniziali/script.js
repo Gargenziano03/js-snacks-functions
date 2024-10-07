19819dff1556d5230ad2921a795b1dfde57927c0
Gargenziano03 <gargenziano03@gmail.com>
2024-10-07 16:10:25 +0200

    add exsercise 1 invoke function

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -1,10 +1,10 @@
 /* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
-let iniziali;
+let iniziali = [];
 
 // Dichiara la funzione qui.
-function estrazione(names) {
+function estrazione(names, iniziali) {
     for (let i = 0; i < names.length; i++) {
         iniziali.push(names[i].charAt(0));
     }
@@ -12,7 +12,6 @@ function estrazione(names) {
 }
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(estrazione(names, iniziali));
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
