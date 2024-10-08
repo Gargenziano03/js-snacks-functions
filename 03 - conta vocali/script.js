672ce7dbf97a9160342a778dcce1fc640a5d8078
Gargenziano03 <gargenziano03@gmail.com>
2024-10-08 10:44:34 +0200

    add exrecise 3 corretto

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -4,9 +4,20 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function ricercaVocali(str){
+    const vocali = ['a','e','i','o','u'];
+    let counter = 0;
+    for (let i = 0; i < str.length; i++) {
+        const element = str[i];
+        if (vocali.includes(element)) {
+            counter++
+        }
+    }return counter
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
+console.log(ricercaVocali(word));
 
 
 
